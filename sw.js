c0aada5049d982357ef44f7257be44032238efa6
Viet Anh NGUYEN <nv2anh@gmail.com>
2018-11-21 16:43:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,22 +2,36 @@
 
 self.addEventListener('install', function(e) {
   console.log('installing');
-  // e.waitUntil(
-  //   caches.open('my-first-cache')
-  //     .then(function(cache) {
-  //       return cache.addAll(['/']);
-  //     })
-  // );
+  
 });
 self.addEventListener('fetch', function(e) {
-  // e.respondWith(
-  //   caches.match(e.request)
-  //     .then(function(res) {
-  //       if (res) {
-  //         return res;
-  //       }
+ 
+});
 
-  //       return fetch(e.request);
-  //     })
-  // );
+self.addEventListener('push', function(event) {
+    var rawData = event.data.json();
+    var notificationTitle = rawData.data.title;
+    var notificationOptions = {
+        body: rawData.data.body,
+        icon: rawData.data.icon,
+        image: rawData.data.image,
+        data: JSON.parse(rawData.data.data),
+    };
+
+    if (rawData.data.received_api) {
+        fetch(rawData.data.received_api, { mode: 'no-cors' });
+    }
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    if (event.notification.data.clicked_api) {
+        fetch(event.notification.data.clicked_api, { mode: 'no-cors' });
+    }
+
+    event.waitUntil(
+        clients.openWindow(event.notification.data.click_action)
+    );
 });
