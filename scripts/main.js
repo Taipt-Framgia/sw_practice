11376408b5c82f0809f8ab351b9e85a503f831d0
Viet Anh NGUYEN <nv2anh@gmail.com>
2018-11-22 10:20:58 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,30 @@
 'use strict';
 
-$( document ).ready(function() {
-    firebase.initializeApp(window.firebaseConfig);
-    const messaging = firebase.messaging();
-    messaging.usePublicVapidKey(window.vapidPublicKey);
+firebase.initializeApp(window.firebaseConfig);
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey(window.vapidPublicKey);
 
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('sw.js').then(function(swReg) {
-                messaging.useServiceWorker(swReg);
-                messaging.requestPermission().then(function () {
-                    messaging.getToken().then(function (currentToken) {
-                        // TODO : Hanle Token
-                        var confirm = confirm("Copy Token to clipboard : " + currentToken);
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('sw.js').then(function(swReg) {
+            messaging.useServiceWorker(swReg);
+            messaging.requestPermission().then(function () {
+                messaging.getToken().then(function (currentToken) {
+                    // TODO : Hanle Token
+                    var confirm = confirm("Copy Token to clipboard : " + currentToken);
 
-                        if (confirm == true) {
-                            document.execCommand("copy");
-                        }
+                    if (confirm == true) {
+                        document.execCommand("copy");
+                    }
 
 
-                    }).catch(function (err) {
-                        console.log(err);
-                    });
                 }).catch(function (err) {
-                    console.log('Unable to get permission to notify.', err);
+                    console.log(err);
                 });
-            })
-        });
-    }
-});
+            }).catch(function (err) {
+                console.log('Unable to get permission to notify.', err);
+            });
+        })
+    });
+}
 
